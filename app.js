43ee1aea82841fe3ccdd0ed17bf16c94547dc352
Gaurang <gaurangjalan1234@gmail.com>
2023-06-22 12:02:26 +0530

    Complete To-Do List

---
+++ app.js
@@ -0,0 +1,48 @@
+//jshinteversion:6  
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+let tasks = [];
+let workItems = [];
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+    
+    let day = date.getDate();
+    res.render("list", {listTitle: day, AddedTasks: tasks});
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", AddedTasks: workItems});
+});
+
+app.post("/", function (req, res) {
+    let task = req.body.newTask;
+    
+    if (req.body.addTask === "Work") {
+        workItems.push(task);
+        res.redirect("/work");
+    } else {
+        tasks.push(task);
+        res.redirect("/");
+    };
+
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.listen(3000, function () {
+    console.log("The server started on port 3000");
+});
